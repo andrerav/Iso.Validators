8305c3942a40e89418e5c39c957c553d808bf4d8
Andreas Ravnestad <andreas.ravnestad@gmail.com>
2015-01-11 18:33:57 +0100

    Added nuspec file, moved nuget packaging script, added automatic build numbering to version string.

--- src/BuildPackages.bat
+++
@@ -1,2 +0,0 @@
-if not exist "./NuGet Packages" mkdir "./NuGet Packages"
-call ".nuget/NuGet.exe" pack Iso.Validators\Iso.Validators.csproj -Build -IncludeReferencedProjects -Properties Configuration=Release -OutputDirectory "./NuGet Packages"
